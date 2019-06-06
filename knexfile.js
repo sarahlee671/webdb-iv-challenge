67ac52fddbbd33429cb13e1ae39d98dcebd11ce3
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-06 10:05:17 -0700

    added knexfile development and moved migrations and seeds to the data folder

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,16 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/recipe.db3', // the folder will be created when we run the migrations
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    // moving migrations to the data folder
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './data/seeds'
     }
   }
-
-};
+}
