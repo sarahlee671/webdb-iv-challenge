2907c7dedd301b12f05879678140b82634afc7b4
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-06 12:33:38 -0700

    added recipe-ingredients table

---
+++ data/migrations/20190606121555_create_receipe-ingredients_table.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema
+        .createTable('recipe_ingredients', tbl => {
+            tbl.increments(); //primary key id
+
+            tbl //foreign key that refers to recipe_id
+                .integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('recipes')
+                .onUpdate('CASCADE');
+
+            tbl //foreign key that refers to ingredient_id
+                .integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('ingredients')
+                .onUpdate('CASCADE')
+
+            tbl //needed quantity of ingredient
+                .integer('quantity')
+                .notNullable()
+        })
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('recipe_ingredients')
+  
+};
