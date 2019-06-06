a61b6ee40d7644b9f4fe87e25177fc986d600aa1
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-06 13:50:00 -0700

    Created index file and server.js file and imported dependencies. Created model and router files for both dishes and recipes and gave server the endpoints

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet')
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+
+const recipesRouter = require('../recipes/recipes-router.js')
+const dishesRouter = require('../dishes/dishes-router.js')
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+server.use('/api/dishes', dishesRouter);
+server.use('/api/recipes', recipesRouter);
+
+
+module.exports = server;
